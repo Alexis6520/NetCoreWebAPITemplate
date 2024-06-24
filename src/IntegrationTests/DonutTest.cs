15099079c64d9cf3a894ae17ff74023f4b356ccf
Alexis <alexis.ortega@anconaautopartes.com>
2024-06-24 15:29:02 -0500

    Obtener todas la donas en prueba de integración

--- src/IntegrationTests/DonutTest.cs
+++ src/IntegrationTests/DonutTest.cs
@@ -1,9 +1,10 @@
-﻿using Domain.Entities;
-using Infrastructure.Persistence;
+﻿using Infrastructure.Persistence;
 using IntegrationTests.Services;
 using Logic.Handlers.DonutHandlers;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Services.Queries.DTOs.DonutDTOs;
+using Services.Wrappers;
 using System.Net.Http.Json;
 
 namespace IntegrationTests
@@ -11,26 +12,6 @@ namespace IntegrationTests
     public class DonutTest(CustomWebAppFactory factory) : BaseTest(factory)
     {
         private const string Url = "Donuts";
-
-        [Fact]
-        public async Task CreateDonut()
-        {
-            // Ejecutar
-            var command = new DonutCreateCommand("Frambuesa", 19);
-            var response = await _client.PostAsJsonAsync(Url, command);
-            response.EnsureSuccessStatusCode();
-
-            // Comprobar que la dona esté en la base de datos 
-            using var scope = _factory.Services.CreateScope();
-
-            using var dbContext = scope.ServiceProvider
-                .GetRequiredService<ApplicationDbContext>();
-
-            var exists = await dbContext.Donuts
-                .AnyAsync(x => x.Name == command.Name);
-
-            Assert.True(exists);
-        }
 
         [Fact]
         public async Task UpdateDonut()
@@ -41,7 +22,11 @@ namespace IntegrationTests
             using var dbContext = scope.ServiceProvider
                 .GetRequiredService<ApplicationDbContext>();
 
-            var id = await CreateTestDonutAsync(dbContext);
+            await CreateDonutAsync();
+
+            var id = await dbContext.Donuts
+                .Select(x => x.Id)
+                .FirstAsync();
 
             // Actualizar por medio de la API
             var command = new DonutUpdateCommand
@@ -63,13 +48,44 @@ namespace IntegrationTests
             Assert.Equal(command.Price, donut.Price);
         }
 
-        private static async Task<int> CreateTestDonutAsync(ApplicationDbContext dbContext)
+        private async Task CreateDonutAsync()
         {
-            var donut = new Donut("Chocolate", 19);
-            await dbContext.Donuts.AddAsync(donut);
-            await dbContext.SaveChangesAsync();
-            dbContext.ChangeTracker.Clear();
-            return donut.Id;
+            // Ejecutar
+            var command = new DonutCreateCommand("Frambuesa", 19);
+            var response = await _client.PostAsJsonAsync(Url, command);
+            response.EnsureSuccessStatusCode();
+
+            // Comprobar que la dona esté en la base de datos 
+            using var scope = _factory.Services.CreateScope();
+
+            using var dbContext = scope.ServiceProvider
+                .GetRequiredService<ApplicationDbContext>();
+
+            var exists = await dbContext.Donuts
+                .AnyAsync(x => x.Name == command.Name);
+
+            Assert.True(exists);
+        }
+
+        [Fact]
+        public async Task GetAll()
+        {
+            var size = 2;
+            var url = $"{Url}?page=1&pageSize={size}";
+            var result = await _client.GetFromJsonAsync<PaginatedList<DonutDTO>>(url);
+            Assert.NotNull(result);
+
+            if (result.TotalCount > 0)
+            {
+                Assert.True(result.Items.Count == size);
+                Assert.True(result.Items.Count <= result.TotalCount);
+                Assert.True(result.PageCount >= 1);
+            }
+            else
+            {
+                Assert.Equal(0, result.TotalCount);
+                Assert.Equal(0, result.PageCount);
+            }
         }
     }
 }
