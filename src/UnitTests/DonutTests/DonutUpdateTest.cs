a236b3be76ca7bbbec8703192a26d425e61a32bf
Alexis <alexis.ortega@anconaautopartes.com>
2024-06-24 10:03:46 -0500

    Pruebas de actualización

--- src/UnitTests/DonutTests/DonutUpdateTest.cs
+++ src/UnitTests/DonutTests/DonutUpdateTest.cs
@@ -1,5 +1,6 @@
 ﻿using Domain.Entities;
 using Logic.Handlers.DonutHandlers;
+using Logic.Validators.DonutValidators;
 using Moq;
 using System.Net;
 
@@ -8,6 +9,28 @@ namespace UnitTests.DonutTests
     [TestClass]
     public class DonutUpdateTest : BaseTest<DonutUpdateHandler>
     {
+        public static IEnumerable<object[]> ValidationData
+        {
+            get
+            {
+                return
+                [
+                    [new DonutUpdateCommand{
+                        Name="",
+                        Price=1
+                    }],
+                     [new DonutUpdateCommand{
+                        Name="",
+                        Price=-1
+                    }],
+                    [new DonutUpdateCommand{
+                        Name="Glaseada original",
+                        Price=-1
+                    }]
+                ];
+            }
+        }
+
         [TestMethod]
         public async Task UpdateDonut()
         {
@@ -40,8 +63,37 @@ namespace UnitTests.DonutTests
             Assert.AreEqual(HttpStatusCode.NoContent, result.StatusCode);
 
             // Comprobar actualización
-            Assert.AreEqual(donut.Name,command.Name);
-            Assert.AreEqual(donut.Price,command.Price);
+            Assert.AreEqual(donut.Name, command.Name);
+            Assert.AreEqual(donut.Price, command.Price);
+        }
+
+        [TestMethod]
+        public async Task UpdateNonExistent()
+        {
+            // Configurar UnitOfWork
+            _unitOfWorkMock.Setup(x => x.Donuts.FindAsync(It.IsAny<object[]>(), default))
+                .Returns(Task.FromResult<Donut?>(null));
+
+            // Simular operación
+            var command = new DonutUpdateCommand();
+            var handler = new DonutUpdateHandler(_unitOfWorkMock.Object, _loggerMock.Object);
+            var result = await handler.Handle(command, default);
+
+            // Comprobar resultado
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.Succeeded);
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+            Assert.IsNotNull(result.Errors);
+            Assert.IsTrue(result.Errors.Any());
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(ValidationData))]
+        public void ValidateCommand(DonutUpdateCommand command)
+        {
+            var validator = new DonutUpdateValidator();
+            var result = validator.Validate(command);
+            Assert.IsFalse(result.IsValid);
         }
     }
 }
