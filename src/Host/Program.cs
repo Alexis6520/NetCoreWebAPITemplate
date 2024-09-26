8382445a5fb4a736886ab3f681a85dee8582db7a
Alexis6520 <alexis_nova@outlook.com>
2024-09-25 21:57:03 -0500

    Provedor de logs de nlog

--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -2,28 +2,53 @@ using Application.Services;
 using Host.Extensions;
 using Host.Middlewares;
 using Infrastructure;
+using NLog;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = LogManager.GetCurrentClassLogger();
 
-builder.Services
-    .AddApplicationServices()
-    .AddInfrastructure();
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.InitializeDatabase();
-}
+    var builder = WebApplication.CreateBuilder(args);
 
-app.UseMiddleware<ExceptionMiddleware>();
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-app.Run();
+    builder.Services
+        .AddApplicationServices()
+        .AddInfrastructure();
+
+    // NLog: Setup NLog for Dependency injection
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.InitializeDatabase();
+    }
+
+    app.UseMiddleware<ExceptionMiddleware>();
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+
+    // needed for  ${aspnet-request-posted-body} with an API Controller.
+    app.UseMiddleware<NLogRequestPostedBodyMiddleware>(
+        new NLogRequestPostedBodyMiddlewareOptions());
+
+    app.MapControllers();
+    app.Run();
+}
+catch (Exception ex)
+{
+    logger.Fatal(ex,"Programa detenido por excepción");
+	throw;
+}
+finally
+{
+    LogManager.Shutdown();
+}
